0f28d81360897313896db26e198e188eb8b15f8b
Leroy <leroypawlick@gmail.com>
2023-06-07 20:42:31 -0700

    created routes and set up link in header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
 import "./App.css";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
+import HomeScreen from "./components/homeComponents/HomeScreen";
+import DetailScreen from "./components/detailComponents/DetailScreen";
+import NewRecipeScreen from "./components/newRecipeComponents/NewRecipeScreen";
+
+const router = createBrowserRouter([
+  { path: "/", element: <HomeScreen />},
+  { path: "/newRecipe", element: <NewRecipeScreen />},
+  { path: "/recipe:id", element: <DetailScreen />},
+
+
+]);
 
 function App() {
   return (
     <div className="App">
       <Header />
-      {/* This is where you will code for some of Part 1. */}
+      <RouterProvider router={router} />
       <Footer />
     </div>
   );
